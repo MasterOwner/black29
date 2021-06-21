d9f22151912dc28932139cf0ba73832a659c7abb
MasterOwner <48113030+MasterOwner@users.noreply.github.com>
2021-06-21 23:42:05 +0800

    嵌套路由

--- src/main.js
+++ src/main.js
@@ -28,6 +28,10 @@ Vue.filter('timeFormate',(val,templateT)=>{
 import index from "./components/index.vue";
 import cart from "./components/shopCar.vue";
 import detail from "./components/detail.vue";
+import container from "./components/container.vue";
+import center from "./components/center.vue";
+import centerdetail from "./components/centerdetail.vue";
+import order from "./components/order.vue";
 
 let routes = [
   {
@@ -45,6 +49,24 @@ let routes = [
   {
     path: "/detail/:id",
     component: detail,
+  },
+  {
+    path: "/container",
+    component: container,
+    children:[
+      {
+        path:'center',
+        component:center
+      },
+      {
+        path:'centerdetail',
+        component:centerdetail
+      },
+      {
+        path:'order',
+        component:order
+      },
+    ]
   },
 ];
 let router = new VueRouter({
